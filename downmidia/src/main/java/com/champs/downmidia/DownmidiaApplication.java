e4a2344d254a1253d7cf46eaba19940de47892f3
champs-8 <fabio.s.dev@outlook.com>
2025-06-03 19:51:26 -0300

    praticando api

---
+++ downmidia/src/main/java/com/champs/downmidia/DownmidiaApplication.java
@@ -0,0 +1,13 @@
+package com.champs.downmidia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownmidiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownmidiaApplication.class, args);
+	}
+
+}
---
+++ downmidia/src/test/java/com/champs/downmidia/DownmidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.champs.downmidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownmidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
